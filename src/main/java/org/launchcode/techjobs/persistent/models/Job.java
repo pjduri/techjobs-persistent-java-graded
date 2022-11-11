c14532fdc5c6f3c91fa013445e96caf72e0a9cf9
Your Name <pjduri@gmail.com>
2022-11-11 01:31:52 -0600

    updated Skill and Job classes with List fields to create many-to-many relationship between them

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -1,18 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Job extends AbstractEntity {
 
     @ManyToOne
     private Employer employer;
-    private String skills;
+
+    @ManyToMany
+    private List<Skill> skills = new ArrayList<>();
 
     public Job() {
     }
 
-    public Job(Employer anEmployer, String someSkills) {
+    public Job(Employer anEmployer, List<Skill> someSkills) {
         super();
         this.employer = anEmployer;
         this.skills = someSkills;
@@ -27,11 +31,11 @@ public class Job extends AbstractEntity {
         this.employer = employer;
     }
 
-    public String getSkills() {
+    public List<Skill> getSkills() {
         return skills;
     }
 
-    public void setSkills(String skills) {
+    public void setSkills(List skills) {
         this.skills = skills;
     }
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,17 +1,33 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
 
     private String description;
 
     public Skill() {
     }
 
-    public Skill(String description) {
+    public Skill(List<Job> jobs, String description) {
+        super();
+        this.jobs = jobs;
         this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
     }
 
     public String getDescription() {
