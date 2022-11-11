360c8da15b44833720f611ddf684d6876b3a510e
Your Name <pjduri@gmail.com>
2022-11-11 00:32:51 -0600

    removed mappedBy parameter from OneToMany annotation

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -15,7 +15,7 @@ public class Employer extends AbstractEntity {
     @Size(min = 3, max = 255)
     private String location;
 
-    @OneToMany(mappedBy = "employer")
+    @OneToMany
     @JoinColumn(name="employer_id")
     private List<Job> jobs = new ArrayList<>();
 
