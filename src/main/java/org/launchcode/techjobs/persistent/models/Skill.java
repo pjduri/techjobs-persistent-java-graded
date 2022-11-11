3b72aeb47f5bbe2670407c7791a1ed4abc79ed4d
Your Name <pjduri@gmail.com>
2022-11-11 15:04:53 -0600

    removed unnecessary setter for jobs list in Skill

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -26,10 +26,6 @@ public class Skill extends AbstractEntity {
         return jobs;
     }
 
-    public void setJobs(List<Job> jobs) {
-        this.jobs = jobs;
-    }
-
     public String getDescription() {
         return description;
     }
